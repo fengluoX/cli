cdc47563849292f0bea07a52704c363f687f4314
luoye <luoyevzhiqiu@gamil.com>
2022-10-22 23:10:18 +0800

    feat: 增加多包管理代码

---
+++ packages/header/src/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// eslint-disable-next-line import/prefer-default-export
+export function Header() {
+    return (
+        <header>
+            Lerna is the original monorepo tool222!
+        </header>
+    );
+}
---
+++ packages/remixapp/app/routes/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from "header";
+
+export default function Index() {
+  return (
+    <>
+      <Header />
+      <div>Content!</div>
+    </>
+  );
+}
